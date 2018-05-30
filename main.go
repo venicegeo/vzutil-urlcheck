34e2a1cbbf73d0063e1cdd7284a51865885d6e5c
wkhunt <wkhunt3@gmail.com>
2018-05-30 08:39:40 -0400

    Can read xlsx files

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/tealeg/xlsx"
 	"github.com/venicegeo/vzutil-urlcheck/nt"
 )
 
@@ -53,6 +54,8 @@ func main() {
 	gitLocation := os.Getenv("CSV_LOCATION")
 	if gitLocation == "" {
 		log.Fatalln("No git location specified")
+	} else if !strings.HasSuffix(gitLocation, ".csv") && !strings.HasSuffix(gitLocation, ".xlsx") {
+		log.Fatalln("The file format cannot be processed")
 	}
 
 	if _, err := os.Stat("work"); os.IsNotExist(err) {
@@ -65,53 +68,77 @@ func main() {
 		}
 	}
 	defer exec.Command("rm", "-rf", "work").Run()
-	dat, err := ioutil.ReadFile("work/" + gitLocation)
+
+	var sheets [][][]string
+	var err error
+	if strings.HasSuffix(gitLocation, ".csv") {
+		dat, e := ioutil.ReadFile("work/" + gitLocation)
+		if e != nil {
+			err = e
+		} else {
+			records, e := csv.NewReader(bytes.NewReader(dat)).ReadAll()
+			sheets = [][][]string{records}
+			err = e
+		}
+	} else {
+		file, e := xlsx.OpenFile("work/" + gitLocation)
+		if e != nil {
+			err = e
+		} else {
+			sheets, err = file.ToSlice()
+		}
+	}
+
 	if err != nil {
 		log.Fatalln(err)
 	}
-	records, err := csv.NewReader(bytes.NewReader(dat)).ReadAll()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	wg := sync.WaitGroup{}
-	wg.Add(len(records))
-	for _, r := range records {
-		go func(row []string) {
-			for _, item := range row {
-				if item == "" || !strings.Contains(item, "http") {
-					continue
-				}
-				parts := split.Split(item, -1)
-				for _, part := range parts {
-					if part == "" {
-						continue
-					}
-					if !strings.HasPrefix(part, "http") {
-						continue
-					}
-					if !strings.HasPrefix(part, "https") {
-						log.Println("[WARNING] Will not run against:", part)
-						continue
-					}
-					header := basic
-					for k, v := range creds {
-						if strings.HasPrefix(part, k) {
-							header = nt.NewHeaderBuilder().AddBasicAuth(v[0], v[1]).GetHeader()
-							break
+	wgSheets := sync.WaitGroup{}
+	wgSheets.Add(len(sheets))
+	for _, s := range sheets {
+		go func(records [][]string) {
+			wgRecords := sync.WaitGroup{}
+			wgRecords.Add(len(records))
+			for _, r := range records {
+				go func(row []string) {
+					for _, item := range row {
+						if item == "" || !strings.Contains(item, "http") {
+							continue
+						}
+						parts := split.Split(item, -1)
+						for _, part := range parts {
+							if part == "" {
+								continue
+							}
+							if !strings.HasPrefix(part, "http") {
+								continue
+							}
+							if !strings.HasPrefix(part, "https") {
+								log.Println("[WARNING] Will not run against:", part)
+								continue
+							}
+							header := basic
+							for k, v := range creds {
+								if strings.HasPrefix(part, k) {
+									header = nt.NewHeaderBuilder().AddBasicAuth(v[0], v[1]).GetHeader()
+									break
+								}
+							}
+							code, _, _, err := nt.HTTP(nt.GET, part, header, nil)
+							if err != nil {
+								fmt.Printf("FAILED %s Code: [%d] Error: [%s]\n", part, code, err.Error())
+							} else if code != 200 && (code > 304 || code < 299) {
+								fmt.Printf("FAILED %s Code: [%d] Error: []\n", part, code)
+							} else {
+								fmt.Printf("PASSED %s Code: [%d]\n", part, code)
+							}
 						}
 					}
-					code, _, _, err := nt.HTTP(nt.GET, part, header, nil)
-					if err != nil {
-						fmt.Printf("FAILED %s Code: [%d] Error: [%s]\n", part, code, err.Error())
-					} else if code != 200 && (code > 304 || code < 299) {
-						fmt.Printf("FAILED %s Code: [%d] Error: []\n", part, code)
-					} else {
-						fmt.Printf("PASSED %s Code: [%d]\n", part, code)
-					}
-				}
+					wgRecords.Done()
+				}(r)
 			}
-			wg.Done()
-		}(r)
+			wgRecords.Wait()
+			wgSheets.Done()
+		}(s)
 	}
-	wg.Wait()
+	wgSheets.Wait()
 }
